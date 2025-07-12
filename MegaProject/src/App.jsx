fbbb7584908df2b63d2812b36c5bbb63f66444d2
Angad Bari <angadbari2@gmail.com>
2025-07-12 23:46:23 +0530

    add Dependancies and .env step

---
+++ MegaProject/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+  console.log("VITE_APPWRITE_URL:", import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+     <h1>React mega project</h1>
+    </>
+  )
+}
+
+export default App
